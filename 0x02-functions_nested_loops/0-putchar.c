af9713a7914f01872fa2f138f540724f3070023c
sirbaass <akpanuforo40@gmail.com>
2023-02-22 15:36:09 -0800

    a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+/**
+ * main: - main function
+ * _putchar: print the alphabeths putchar
+ * Return: always 0
+ */
 
 int main(void)
 {
-    char word[] = "_putchar";
-    int i;
-    for (i = 0; word[i] != '\0'; i++) 
-    {
-        putchar(word[i]);
-    }
-    
-    putchar('\n');
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
     return 0;
 }
 
