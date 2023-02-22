1d2a9307cc94dc13155b31531a1dba04223e68b1
sirbaass <akpanuforo40@gmail.com>
2023-02-22 14:00:58 -0800

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char word[] = "_putchar";
+    int i;
+    for (i = 0; word[i] != '\0'; i++) 
+    {
+        putchar(word[i]);
+    }
+    
+    putchar('\n');
+    return 0;
+}
+
