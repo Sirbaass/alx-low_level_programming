bb5bc66e09c3c758d25bb2967d2d44f305426243
sirbaass <akpanuforo40@gmail.com>
2023-02-22 15:43:01 -0800

    A function that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,18 @@
-#include <stdio.h>
+#include"main.h"
 /**
- * main: - main function
- * _putchar: print the alphabeths putchar
- * Return: always 0
+ * main - Print _putchar
+ * Return: Always 0
  */
-
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return 0;
+	putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
-
